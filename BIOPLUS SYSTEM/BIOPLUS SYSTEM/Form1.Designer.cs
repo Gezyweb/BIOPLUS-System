4c29bb19cb6eeb6d2a6d7805acbd9828e0ed0a0c
Gezyweb <119985813+Gezyweb@users.noreply.github.com>
2023-04-19 11:30:42 +0800

    Add files via upload

---
+++ BIOPLUS SYSTEM/BIOPLUS SYSTEM/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace BIOPLUS_SYSTEM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.signinBtn = new System.Windows.Forms.Button();
+            this.registerBtn = new System.Windows.Forms.Button();
+            this.CreateLbl = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // signinBtn
+            // 
+            this.signinBtn.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.signinBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.signinBtn.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.signinBtn.FlatAppearance.BorderSize = 0;
+            this.signinBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.signinBtn.Font = new System.Drawing.Font("Segoe UI Black", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.signinBtn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.signinBtn.Location = new System.Drawing.Point(300, 227);
+            this.signinBtn.Margin = new System.Windows.Forms.Padding(0);
+            this.signinBtn.Name = "signinBtn";
+            this.signinBtn.Size = new System.Drawing.Size(118, 32);
+            this.signinBtn.TabIndex = 1;
+            this.signinBtn.Text = "SIGN IN";
+            this.signinBtn.UseVisualStyleBackColor = false;
+            this.signinBtn.Click += new System.EventHandler(this.signinBtn_Click);
+            // 
+            // registerBtn
+            // 
+            this.registerBtn.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.registerBtn.FlatAppearance.BorderSize = 0;
+            this.registerBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.registerBtn.Font = new System.Drawing.Font("Segoe UI Black", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.registerBtn.ForeColor = System.Drawing.Color.White;
+            this.registerBtn.Location = new System.Drawing.Point(299, 297);
+            this.registerBtn.Name = "registerBtn";
+            this.registerBtn.Size = new System.Drawing.Size(119, 31);
+            this.registerBtn.TabIndex = 0;
+            this.registerBtn.Text = "REGISTER";
+            this.registerBtn.UseVisualStyleBackColor = false;
+            // 
+            // CreateLbl
+            // 
+            this.CreateLbl.AutoSize = true;
+            this.CreateLbl.BackColor = System.Drawing.Color.Transparent;
+            this.CreateLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CreateLbl.Location = new System.Drawing.Point(298, 278);
+            this.CreateLbl.Name = "CreateLbl";
+            this.CreateLbl.Size = new System.Drawing.Size(124, 17);
+            this.CreateLbl.TabIndex = 1;
+            this.CreateLbl.Text = "Create an account";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(348, 259);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "or";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(278, 54);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(161, 161);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(718, 464);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CreateLbl);
+            this.Controls.Add(this.registerBtn);
+            this.Controls.Add(this.signinBtn);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button signinBtn;
+        private System.Windows.Forms.Button registerBtn;
+        private System.Windows.Forms.Label CreateLbl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
